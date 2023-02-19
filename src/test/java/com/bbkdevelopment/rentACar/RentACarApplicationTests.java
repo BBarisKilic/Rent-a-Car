483123ba3d8e23db955be88b5b115e1251b2f121
Bulent Baris Kilic <bulentbariskilic@gmail.com>
2023-02-19 23:18:23 +0100

    feat: create project

---
+++ src/test/java/com/bbkdevelopment/rentACar/RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbkdevelopment.rentACar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
